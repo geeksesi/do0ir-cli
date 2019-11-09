b6c3d26c982db0c8a4841103ffc73710acd98862
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-11-09 10:21:54 +0330

    it's work but has problem with Json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate reqwest;
+
+use std::collections::HashMap;
+use std::env;
+
+fn get_request() -> Result<(), Box<std::error::Error>> {
+    let client = reqwest::Client::new();
+    let params = [("link", "http://google.com")];
+    let resp = client
+        .post("https://do0.ir/post/sD9qHKZaUC7g/2.5/code")
+        .form(&params)
+        .send()?
+        .json()?;
+    println!("{:#?}", resp);
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    get_request().expect("could not read file");
 }
