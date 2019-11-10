fae45f89df907f6b3c0a386d609030eaa18f2569
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-11-10 08:16:51 +0330

    it's work :)) ( but it's not good )

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
 extern crate reqwest;
-
-use std::collections::HashMap;
+use serde::de::Deserializer;
+use serde::{Deserialize, Serialize};
 use std::env;
 
-fn get_request() -> Result<(), Box<std::error::Error>> {
+#[derive(Debug, Serialize, Deserialize)]
+struct Do0result {
+    success: bool,
+    #[serde(deserialize_with = "parse_null")]
+    error: String,
+    #[serde(deserialize_with = "parse_null")]
+    address: String,
+    #[serde(deserialize_with = "parse_null")]
+    short: String,
+}
+
+fn parse_null<'de, D>(d: D) -> Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Deserialize::deserialize(d).map(|x: Option<_>| x.unwrap_or("null".to_string()))
+}
+
+fn get_request() -> Result<Do0result, reqwest::Error> {
     let client = reqwest::Client::new();
     let params = [("link", "http://google.com")];
-    let resp = client
+    let response: Do0result = client
         .post("https://do0.ir/post/sD9qHKZaUC7g/2.5/code")
         .form(&params)
         .send()?
         .json()?;
-    println!("{:#?}", resp);
-    Ok(())
+
+    // println!("{:#?}", response);
+    // decode_me(resp);
+    Ok(response)
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    get_request().expect("could not read file");
+    let _args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    let do0_answer: Do0result = get_request().expect("could not read file");
+    // do0_answer.short = 
+    // println!("length : {}", do0_answer.short)
+    println!("Short link is https://do0.ir/{}", do0_answer.short)
 }
