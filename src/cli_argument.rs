b729285c82e0c3640d170f24d1420e72b8863d4a
geeksesi <geeksesi@gmail.com>
2019-12-06 19:15:50 +0330

    will detect clipboard also in --no-service

--- src/cli_argument.rs
+++ src/cli_argument.rs
@@ -1,7 +1,19 @@
+extern crate clipboard;
+
 use std::env;
 use std::result::Result;
 
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
 use crate::validation::check_is_url;
+
+fn get_link() -> String {
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+
+    let link : String = check_is_url(ctx.get_contents().unwrap().to_string());
+    return link;
+}
+
 #[derive(Debug)]
 pub struct CliArguments {
     pub link: String,
@@ -25,6 +37,9 @@ impl CliArguments {
             }else if check_is_url(arg.to_string()) != "".to_string() {
                 link = arg.to_string();
             }
+        }
+        if link == "null" {
+            link = get_link();
         }
         return Ok(CliArguments {
             link: link,
